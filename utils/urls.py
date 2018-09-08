bdda784f287a8d8161db2af54ff6bb95fca938df
nguyenhderrick <nguyenhderrick@gmail.com>
2018-09-08 12:57:55 -0700

    Create urls.py

---
+++ utils/urls.py
@@ -0,0 +1,26 @@
+doppelganger = "http://onenightultimate.com/wp-content/uploads/2015/08/doppleganger.jpg"
+werewolf = "http://onenightultimate.com/wp-content/uploads/2015/08/werewolf.jpg"
+minion = "http://onenightultimate.com/wp-content/uploads/2015/08/minion.jpg"
+mason = "http://onenightultimate.com/wp-content/uploads/2015/08/mason.png"
+seer = "http://onenightultimate.com/wp-content/uploads/2015/08/seer.jpg"
+robber = "http://onenightultimate.com/wp-content/uploads/2015/08/robber.jpg"
+troublemaker = "http://onenightultimate.com/wp-content/uploads/2015/08/troublemaker.jpg"
+drunk = "http://onenightultimate.com/wp-content/uploads/2015/08/drunk.jpg"
+insomniac = "http://onenightultimate.com/wp-content/uploads/2015/08/insomniac.jpg"
+tanner = "http://onenightultimate.com/wp-content/uploads/2015/08/tanner.jpg"
+hunter = "http://onenightultimate.com/wp-content/uploads/2015/08/hunter.jpg"
+villager = "http://onenightultimate.com/wp-content/uploads/2015/08/villager.jpg"
+center_img = "https://cf.geekdo-images.com/medium/img/8kb7wc3Pdn2MTsR8fHe3JLzfVYM=/fit-in/500x500/filters:no_upscale()/pic2360783.jpg"
+
+role = {'Doppelganger' : "http://onenightultimate.com/wp-content/uploads/2015/08/doppleganger.jpg",
+        'Werewolf' : "http://onenightultimate.com/wp-content/uploads/2015/08/werewolf.jpg",
+        'Minion' : "http://onenightultimate.com/wp-content/uploads/2015/08/minion.jpg",
+        'Mason' : "http://onenightultimate.com/wp-content/uploads/2015/08/mason.png",
+        'Seer' : "http://onenightultimate.com/wp-content/uploads/2015/08/seer.jpg",
+        'Robber' : "http://onenightultimate.com/wp-content/uploads/2015/08/robber.jpg",
+        'Troublemaker' : "http://onenightultimate.com/wp-content/uploads/2015/08/troublemaker.jpg",
+        'Drunk' : "http://onenightultimate.com/wp-content/uploads/2015/08/drunk.jpg",
+        'Insomniac' : "http://onenightultimate.com/wp-content/uploads/2015/08/insomniac.jpg",
+        'Tanner' : "http://onenightultimate.com/wp-content/uploads/2015/08/tanner.jpg",
+        'Hunter' : "http://onenightultimate.com/wp-content/uploads/2015/08/hunter.jpg",
+        'Villager' : "http://onenightultimate.com/wp-content/uploads/2015/08/villager.jpg"}
